e6a105e1fab26110d59dd36804fec799cda19ff8
Är detta mitt namn? <87585550+OMGOMGplays@users.noreply.github.com>
2025-05-19 14:37:00 +0200

    Version 1.3

--- src/Program.cs
+++ src/Program.cs
@@ -234,7 +234,7 @@ public class Program
 
             // Write some information beforehand
             sw.WriteLine("//");
-            sw.WriteLine("// THIS FILE WAS AUTOMATICALLY TRANSLATED THROUGH VMT2VMAT V.1.2");
+            sw.WriteLine("// THIS FILE WAS AUTOMATICALLY TRANSLATED THROUGH VMT2VMAT V.1.3");
             sw.WriteLine("// IF THERE ARE ANY ISSUES WITH THE PROVIDED TRANSLATION; CONTACT LOKI");
             sw.WriteLine("//");
             sw.WriteLine($"// INFO: TEXTURE FILE EXTENSION: \".{FileExtension}\", ENGINE VERSION: \"{Version}\"");
